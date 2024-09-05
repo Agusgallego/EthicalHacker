edd4891e3b8186f5cfd346d7ed81b7b234abdcb7
AgusGallego <166161311+Agusgallego@users.noreply.github.com>
2024-09-04 23:29:24 -0300

    modulos y paquetes

---
+++ CursoHackingPython/01-Introducción/12-Modulos-paquetes.py
@@ -0,0 +1,16 @@
+
+#? QUÉ ES LA PROGRAMACIÓN MODULAR? 
+# proceso de dividir una tarea grande y dificil de manejar, en subtareas o modulos separados mas pequeños y manejables
+#ejemplo: funciones y clases -> que permiten aplicar MODULARIZACION. Generamos fragemntos de código mas pequeños
+#? Qué es un módulo -> archivo que contiene codigo en python y la extención .py
+#ejemplo, tenemos un archivo llamado => 10-POO.py => lo importamos a este archivo "12-Modulos-paquetes.py"
+#* import 10-POO        -> importamos un modulo
+#* 10-POO.funcion()    -> utilizamos una funcion del modulo importado
+
+# para poder importar modulos, deben estar en la #!MISMA RUTA
+
+#*  form 10-POO import funcion_poo -> SI QUEREMOS IMPORTAR UNA SOLA ESTRUCTURA DE DATOS
+#*  form 10-POO import *  -> importa absolutamente todo del archivo 10-POO
+#? import 10-POO as m1  -> queremos importar con un "alias" de forma reducida
+
+#!                             Principalmente utilizamos esto para MODULOS de python
